1c09b4afa5d5e6cbe520ff6cdfea69e2714fecfd
insshubh <insshubh22@gmail.com>
2025-10-13 02:10:16 +0530

    Initial Setup of the Bitly URL Shortner Added models (USER, MAPPING and CLickEvent

---
+++ src/main/java/com/url/shortner/models/ClickEvent.java
@@ -0,0 +1,19 @@
+package com.url.shortner.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+public class ClickEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url-mapping_id")
+    private URLMapping urlMapping;
+}
